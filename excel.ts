aa79820076fb5f5ce23c63784ab7a1503085ef0e
DENNIS-CODES <dennistrevor06@gmail.com>
2021-08-28 12:08:11 +0300

    authenticating google sheet

---
+++ excel.ts
@@ -0,0 +1,134 @@
+import { GoogleSpreadsheet, GoogleSpreadsheetCell, GoogleSpreadsheetWorksheet } from 'google-spreadsheet';
+
+if (!process.env.GOOGLE_KEY && !process.env.GOOGLE_GMAIL) {
+    `GOOGLE_KEY && GOOGLE_GMAIL must be defined in your .env file`
+}
+
+// console.log(process.env.GOOGLE_GMAIL)
+// console.log(process.env.GOOGLE_KEY!.replace(/\\n/gm, '\n'))
+
+export class GoogleSheet {
+    readonly doc: any
+    sheet1: GoogleSpreadsheetWorksheet | undefined
+    sheet2: GoogleSpreadsheetWorksheet | undefined
+    sheet3: GoogleSpreadsheetWorksheet | undefined
+    sheet4: GoogleSpreadsheetWorksheet | undefined
+    sheet6: GoogleSpreadsheetWorksheet | undefined
+
+    b2: GoogleSpreadsheetCell | undefined
+    b3: GoogleSpreadsheetCell | undefined
+    c2: GoogleSpreadsheetCell | undefined
+    c3: GoogleSpreadsheetCell | undefined
+    readonly header_to_key!: object;
+    readonly spread_header_to_key!: object;
+    readonly spread_live!: object;
+
+    constructor(
+        doc_id: string = '1rH4D6yIB9NRNZDM5z_1keUECn62casZzURDdKpUfUuw'
+    ) {
+        this.doc = new GoogleSpreadsheet(doc_id)
+
+        // console.log(this.doc)
+
+        this.header_to_key = {
+            "Bid Price": 'bid_price',
+            "Ask Price": 'ask_price',
+            "Time": 'timestamp',
+        }
+
+        this.spread_header_to_key = {
+            "Long Spread": 'longSpread',
+            "Short Spread": 'shortSpread',
+            "Time": 'timestamp',
+        }
+
+        this.spread_live = {
+            "Contract": "contract",
+            "Long Spread": 'longSpread',
+            "Short Spread": 'shortSpread'
+        }
+    }
+
+    setup = async () => {
+        await this.doc.useServiceAccountAuth({
+            client_email: process.env.GOOGLE_GMAIL!,
+            private_key: process.env.GOOGLE_KEY!
+        })
+
+        await this.doc.loadInfo();
+
+        this.sheet1 = this.doc.sheetsByIndex[0];
+        this.sheet2 = this.doc.sheetsByIndex[1]
+        this.sheet3 = this.doc.sheetsByIndex[2]
+        this.sheet4 = this.doc.sheetsByIndex[3]
+        this.sheet6 = this.doc.sheetsByIndex[5]
+
+    
+
+
+        // console.log("Sheet ", this.sheet1)
+
+        // await this.sheet1?.clear()
+        // await this.sheet2?.clear()
+        // await this.sheet3?.clear()
+        // await this.sheet4?.clear()
+        // await this.sheet6?.clear()
+
+
+        this.sheet1?.setHeaderRow(Object.keys(this.header_to_key))
+        this.sheet2?.setHeaderRow(Object.keys(this.header_to_key))
+
+        this.sheet3?.setHeaderRow(Object.keys(this.spread_header_to_key))
+        this.sheet4?.setHeaderRow(Object.keys(this.spread_header_to_key))
+        this.sheet6?.setHeaderRow(Object.keys(this.spread_live))
+    }
+
+    insertTo1 = async (record: any[]) => {
+        try {
+            // console.log("inserting rows")
+            // console.log(record)
+            await this.sheet1?.addRow(record, { raw: true, insert: true })
+        } catch (error) {
+            console.log("Error adding data to sheet ", error)
+        }
+    }
+
+    insertTo2 = async (record: any[]) => {
+        try {
+            // console.log("inserting rows")
+            // console.log(record)
+            await this.sheet2?.addRow(record, { raw: true, insert: true })
+        } catch (error) {
+            console.log("Error adding data to sheet ", error)
+        }
+    }
+
+    insertTo3 = async (record: any[]) => {
+        try {
+            // console.log("inserting rows")
+            // console.log(record)
+            await this.sheet3?.addRow(record, { raw: true, insert: false })
+        } catch (error) {
+            console.log("Error adding data to sheet ", error)
+        }
+    }
+
+    insertTo4 = async (record: any[]) => {
+        try {
+            // console.log("inserting rows")
+            // console.log(record)
+            await this.sheet4?.addRow(record, { raw: true, insert: false })
+        } catch (error) {
+            console.log("Error adding data to sheet ", error)
+        }
+    }
+
+    insertTo6 = async (record: any[]) => {
+        try {
+            await this.sheet6?.addRow(record, { raw: true, insert: false })
+        } catch (error) {
+            console.log("Error adding data to sheet ", error)
+        }
+    }
+
+}
--- index.ts
+++ index.ts
@@ -1,79 +1,171 @@
 import axios from "axios";
-import cron, { schedule } from 'node-cron'
+import { schedule } from 'node-cron'
 import * as fs from 'fs'
-import { TOKENS } from './src/tokens'
-import { ExcelSheet } from "./excell";
+import { GoogleSheet } from "./excel";
+import { GoogleSpreadsheet, GoogleSpreadsheetWorksheet } from "google-spreadsheet";
 
 const ExcelJS = require('exceljs');
 // require('dotenv').config();
 
 const writeFile = fs.writeFile
 
-let CHK_INTERVAL = 10
-let PUSH_INTERVAL = 1
+let CHK_INTERVAL = 1
+let symbols = [
+    'BTC/USD',
+    'BTC-PERP'
+]
+ 
 
-let excel = new ExcelSheet()
+let sheet6: GoogleSpreadsheetWorksheet | undefined
+let spread_live!: object;
+
+let doc_id: string = '1JBKkv0oT8K2qIND9V5yyhBnqQzbzKZ2H8XxGOBnFcRA'
+
+let doc = new GoogleSpreadsheet(doc_id)
 
 
-const market_data = async () => {
-    await excel.setup().then((res) => {
-        console.log('Excell set up');  
-    }).catch((err) => console.log(err)
-    )
+let gs = new GoogleSheet()
 
-    for (let i = 0; i < TOKENS.length; i++) {
-        let candle = TOKENS[i]
-        schedule(`*/${CHK_INTERVAL} * * * * *`, async () => {
-           // console.log("running a task every 10 second")
-            let response = await axios({
-                method: 'GET',
-                url: `https://ftx.com/api/markets/${candle}`,
-                // headers: {
-                //     'X-FTX-Key': process.env.API_KEY
-                // }
+let market_data = async () => {
+    await gs.setup().then((res) => {
+        console.log('Google sheet has been setup')
+    }).catch((err) => {
+        console.log('Error: ', err)
+    })
 
-            })
-            // console.log(response.data);
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_GMAIL!,
+        private_key: process.env.GOOGLE_KEY!
+    })
 
-            let bid = response.data.result.bid
-            let ask = response.data.result.ask
-            console.log('Bid Price: ',bid);
-            console.log('Ask Price: ',ask);
+    await doc.loadInfo();
 
-            if(response.data.result.name == "BTC/USD") {
-            
-                
-            excel.add_row_usd([
-                `${bid}`,
-                `${ask}`,
-            ]).then(() => console.log('Row saved')
-            )}
+    let ask1 = 0
+    let ask2 = 0
+    let bid1 = 0
+    let bid2 = 0
 
-            if(response.data.result.name == "BTC-PERP") {
-            
-                
-                excel.add_row_perp([
-                    `${bid}`,
-                    `${ask}`,
-                ]).then(() => console.log('Row saved')
-                )}
-            // let data = response.data
-            // var options = {
-            //     headers: ''
-            // }
-            // let excelData = await ExcelJS.exceljs(data, options)
-            // writeFile(`./excel_data/data.xlsx`, excelData, (err: any) => {
-            //     if (err) {
-            //         // Handle the errors
-            //         console.log(err);
-            //         throw new Error(err);
-            //     }
-            //     console.log(` Datasaved into excel files successfully😊`);
-            // })
-
-        })
-        
+    spread_live = {
+        "Contract": "contract",
+        "Long Spread": 'longSpread',
+        "Short Spread": 'shortSpread'
     }
+
+    sheet6 = doc.sheetsByIndex[5]
+
+   
+
+    await sheet6.loadCells()
+
+    let b2 = sheet6?.getCellByA1('B2')
+    let b3 = sheet6?.getCellByA1('B3')
+    let c2 = sheet6?.getCellByA1('C2')
+    let c3 = sheet6?.getCellByA1('C3')
+
+    schedule(`*/${CHK_INTERVAL} * * * * *`, async () => {
+        // console.log(askPrice ,':',bidPrice )
+        symbols.forEach(async coins => {
+            try {
+                let response = await axios({
+                    method: "GET",
+                    url: `https://ftx.com/api/markets/${symbols}`,
+                    params: {
+                        symbol: `${coins}`,
+                    }
+                })
+    // for (let i = 0; i < TOKENS.length; i++) {
+    //     let candle = TOKENS[i]
+    //     schedule(`*/${CHK_INTERVAL} * * * * *`, async () => {
+    //        // console.log("running a task every 10 second")
+    //         let response = await axios({
+    //             method: 'GET',
+    //             url: `https://ftx.com/api/markets/${candle}`,
+    //             // headers: {
+    //             //     'X-FTX-Key': process.env.API_KEY
+    //             // }
+
+    //         })
+    //         // console.log(response.data);
+
+            let idata = response.data.result
+
+                const askPrice = idata[0].ask_price
+                const bidPrice = idata[0].bid_price
+                const timestamp = (await response).data.time_now
+
+                // console.log(`${bidPrice} :${askPrice} : ${timestamp}`);
+
+                let sheetData = [
+                    `${bidPrice}`,
+                    `${askPrice}`,
+                    `${new Date(timestamp * 1000).toUTCString()}`,
+                ]
+
+                // console.log('pricee: ', coins == 'BTCUSD', coins)
+                if (coins == 'BTCUSD') {
+                    ask1 = askPrice
+                    bid1 = bidPrice
+
+                    gs.insertTo1(sheetData)
+                    // console.log('askPrice', ask1)
+                }
+                if (coins == 'BTCUSDT') {
+                    ask2 = askPrice
+                    bid2 = bidPrice
+
+                    // console.log('bidPrice2', bid2)
+                    gs.insertTo2(sheetData)
+                }
+                // console.log('askPrice1', ask1)
+                let shortSpread = (bid1 - ask2)
+                let longSpread = (ask1 - bid2)
+
+
+                let sPread = (bid2 - ask1)
+                let lspread = (ask2 - bid1)
+
+                let spreadData = [
+                    `${longSpread}`,
+                    `${shortSpread}`,
+                    `${new Date(timestamp * 1000).toUTCString()}`,
+                ]
+
+                gs.insertTo3(spreadData)
+
+                let spreadData2 = [
+                    `${lspread}`,
+                    `${sPread}`,
+                    `${new Date(timestamp * 1000).toUTCString()}`,
+                ]
+
+                gs.insertTo4(spreadData2)
+
+
+
+                // if(coins == 'BTCUSD'){}
+                b2.value = longSpread
+                b3.value = shortSpread
+                c2.value = lspread
+                c3.value = sPread
+
+                await sheet6?.saveUpdatedCells()
+
+                let liveSpread = [
+                    `${coins}`,
+
+                ]
+
+            } catch (error) {
+                throw new Error;
+
+            }
+
+        });
+
+
+
+    })
+
 }
     
     
